844962c34957d0d003d8c25ca9aa6dd53970531c
Alexander Tseung <alextsg@gmail.com>
2015-01-19 20:04:37 -0800

    Added server functionality

--- client/app/app.js
+++ client/app/app.js
@@ -14,6 +14,7 @@
       40: 'DOWN'
     };
     var green = false;
+    var correct;
 
     this.newAnswers = function(){
       console.log(this.round);
@@ -35,17 +36,17 @@
       }
       if (this.keys.length === this.round){
         if (this.compare(this.keys, this.round)){
-          console.log("correct");
+          this.correct = true;
           this.newAnswers();
         } else {
-          console.log("wrong");
+          this.correct = false;
+          this.resetGame();
         }
       }
     }
 
     this.compare = function(keys, round){ 
       console.log(keys);
-      console.log(round);
       for (var i = 0; i < keys.length; i++){
         if (keys[i] !== this.answers[i]) return false;
         else this.score++;
@@ -73,6 +74,15 @@
       var refresh = $interval(refreshAnswer.bind(this),1000);
     };
 
+    this.resetGame = function(){
+      this.keys = [];
+      this.round = 0;
+      this.answers = [];
+      this.displayed = "--";
+      this.score = 0;
+      this.newAnswers();
+      this.correct;
+    };
 
   });
 
--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 var express   = require('express'),
-    mongoose  = require('mongoose');
+    mongoose  = require('mongoose'),
+    path = require('path');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/storeapp'); // connect to mongo database named shortly
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(path.join(__dirname, '../client')));
 
 app.get('/', function(req, res){
-  // res.sendfile(__dirname + '/../client/app/index.html', {'root': '/../'});
-  res.sendfile('index.html', {'root': '../client/app/'});
+  res.sendFile(path.join(__dirname + '/../client/index.html'));
 })
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
+
+
+
+//mongoose.connect('mongodb://localhost/storeapp'); // connect to mongo database named shortly
